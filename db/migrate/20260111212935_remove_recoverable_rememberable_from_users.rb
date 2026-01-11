bb74a5692263a84f03f4e6d6d65d69ed993e017f
ghdenisa <gherghiceanudenisa@yahoo.com>
2026-01-11 23:33:39 +0200

    Adds devise gem for user authentication

---
+++ db/migrate/20260111212935_remove_recoverable_rememberable_from_users.rb
@@ -0,0 +1,8 @@
+class RemoveRecoverableRememberableFromUsers < ActiveRecord::Migration[8.1]
+  def change
+    remove_index :users, :reset_password_token
+    remove_column :users, :reset_password_token, :string
+    remove_column :users, :reset_password_sent_at, :datetime
+    remove_column :users, :remember_created_at, :datetime
+  end
+end
